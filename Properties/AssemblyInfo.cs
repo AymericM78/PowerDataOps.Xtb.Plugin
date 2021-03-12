f2bfd220e9b2735f71f3c39a3cccb5a541e08cc5
Aymeric <amouille@live.com>
2021-03-12 11:29:07 +0100

    Include images and new version for XTB validation

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.3.9.1802")]
-[assembly: AssemblyFileVersion("2021.3.9.1802")]
+[assembly: AssemblyVersion("2021.3.12.1100")]
+[assembly: AssemblyFileVersion("2021.3.12.1100")]
